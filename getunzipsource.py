cc411cd8a77b3ed90b70a4a45a3eff220d51055b
mikeAdamss <mikelovesbooks@gmail.com>
2021-05-19 12:07:12 +0100

    basic python downloader

---
+++ getunzipsource.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import shutil
+
+from google.cloud import storage
+from zipfile import ZipFile
+
+# Remove left overs from last run
+if os.path.isdir("source"):
+    shutil.rmtree("source")
+
+def get_client():
+    """
+    Get a storage client
+    """
+    try:
+        storage_client = storage.Client()
+    except Exception as err:
+        raise Exception("Unable to get storage client. Aborting operation:") from err
+        
+    return storage_client
+
+def get_unzip_source(zip_location):
+    storage_client = get_client()
+
+    bucket = storage_client.get_bucket('data-source-bucket-ons')
+
+    blob = bucket.get_blob('remote/path/to/file.txt')
+    blob.download_to_filename(zip_location)
+
+    with ZipFile(blob.name, 'r') as zipObj:
+        zipObj.extractall()
+
+if __name__ == "__main__":
+    zip_location = sys.argv[1]
+    get_unzip_source(zip_location)
